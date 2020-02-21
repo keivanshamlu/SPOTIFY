746adb7da437c9a17d21c396428d43f8327337c1
k.shamloo <k.shamloo@mydigipay.com>
2020-02-21 16:09:47 +0330

    domain module added

--- buildSrc/src/main/java/com/shamlu/app/android/buildsrc/Dependencies.kt
+++ buildSrc/src/main/java/com/shamlu/app/android/buildsrc/Dependencies.kt
@@ -205,4 +205,6 @@ object Modules {
     const val app = ":app"
     const val common = ":common"
     const val navigation = ":navigation"
+    const val di = ":di"
+    const val domain = ":domain"
 }
